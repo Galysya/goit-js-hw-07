be08dc5230d320ebac257f523ecfd4f4921b4a18
Galysya <galiapechil@ukr.net>
2023-11-03 16:36:05 +0200

    g

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,56 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// function (galleryItems) {
+
+// }
+
+
+
+const galleries = document.querySelector(".gallery");
+let galleryImage = "";
+for (const item of galleryItems) {
+  galleryImage += `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+}
+galleries.insertAdjacentHTML( 'afterbegin', galleryImage );
+
+console.log(galleries);
+
+// 2
+
+galleries.addEventListener('click', onGalleryClick)
+
+function onGalleryClick(e) {
+    e.preventDefault()
+    const url = e.target.dataset.source;
+
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show()
+
+
+const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`)
+
+instance.show()
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleries = document.querySelector(".gallery");
+let galleryImage = "";
+for (const item of galleryItems) {
+  galleryImage += 
+  `<li class="gallery__item">
+     <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}"
+       />
+     </a>
+  </li>`
+}
+galleries.insertAdjacentHTML( 'afterbegin', galleryImage );
+
+console.log(galleries);
+
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay: 250 });
+
+
+
+
+
